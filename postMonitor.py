12699465ca05b4bc469b8a9a4b5dd26fc7a8f477
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-09-07 17:13:28 -0700

    everyone likes colors

--- postMonitor.py
+++ postMonitor.py
@@ -11,6 +11,14 @@ import logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
+class colors:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+
 app.url_map.add(Rule('/', endpoint='index'))
 app.url_map.add(Rule('/<path:path>', endpoint='other'))
 
@@ -18,45 +26,41 @@ app.url_map.add(Rule('/<path:path>', endpoint='other'))
 @app.endpoint('other')
 def catch_all(path=None):
 	if path == "" or path == None:
-		print(time.strftime('%Y-%m-%d %H:%M:%S')+' - ['+request.method+'] / - '+request.remote_addr)
+		print(colors.OKBLUE+time.strftime('%Y-%m-%d %H:%M:%S')+colors.ENDC+' - ['+colors.HEADER+request.method+colors.ENDC+'] '+colors.WARNING+'/'+colors.ENDC+' - '+colors.FAIL+request.remote_addr+colors.ENDC)
 	else:
-		print(time.strftime('%Y-%m-%d %H:%M:%S')+' - ['+request.method+'] /'+path+' - '+request.remote_addr)
+		print(colors.OKBLUE+time.strftime('%Y-%m-%d %H:%M:%S')+colors.ENDC+' - ['+colors.HEADER+request.method+colors.ENDC+'] /'+colors.WARNING+path+colors.ENDC+' - '+colors.FAIL+request.remote_addr+colors.ENDC)
 
 	if showHeaders == True:
-		print("\t[Header]")
+		print("\t["+colors.OKGREEN+"Header"+colors.ENDC+"]")
 		for i in request.headers:
 			print("\t\t"+i[0]+": "+i[1])
 
 	if request.args:
-		print("\t[URL Variables]")
-		#pprint.pprint(request.args)
+		print("\t["+colors.OKGREEN+"URL Variables"+colors.ENDC+"]")
 		for i in request.args:
 			print("\t\t"+i+": "+request.args[i])
 
 	if request.headers['Content-Type'].split(';')[0] == "multipart/form-data" and request.form:
-		print("\t[Multipart form-data]")
-		#pprint.pprint(request.form)
+		print("\t["+colors.OKGREEN+"Multipart form-data"+colors.ENDC+"]")
 		for i in request.form:
 			print("\t\t"+i+": "+request.form[i])
 
 	if request.headers['Content-Type'].split(';')[0] == "application/x-www-form-urlencoded" and request.form:
-		print("\t[URL Encoded form-data]")
-		#pprint.pprint(request.form) 
+		print("\t["+colors.OKGREEN+"URL Encoded form-data"+colors.ENDC+"]")
 		for i in request.form:
 			print("\t\t"+i+": "+request.form[i])
 
 	if request.headers['Content-Type'].split(';')[0] == "application/json" and request.json:
-		print("\t[JSON]")
+		print("\t["+colors.OKGREEN+"JSON"+colors.ENDC+"]")
 		for i in json.dumps(request.json, indent=2, sort_keys=False, separators=(',', ': ')).split("\n"):
 			print("\t\t"+i)
 
 	if request.headers['Content-Type'].split(';')[0] == "text/plain":
-		print("\t[Plain text]")
-		#print ("\t"+request.data)
+		print("\t["+colors.OKGREEN+"Plain text"+colors.ENDC+"]")
 		for i in request.data.split("\n"):
 			print("\t\t"+i)
 
-	return "^__^"
+	return "^__^\n"
 
 if __name__ == "__main__":
 	host = "192.168.1.130"
